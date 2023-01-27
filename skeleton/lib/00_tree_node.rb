1bf04b5cd093452e6ca17ff4716e10198f0b73a3
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-01-27 15:33:38 -0500

    brainstorming bfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -21,4 +21,36 @@ class PolyTreeNode
         raise "error" if child.parent == nil
         child.parent=(nil) 
     end
+
+    def bfs(node, target)
+        # queue = [node]
+
+        # if queue.shift == target
+        #     return queue.shift
+        # else
+        #     queue << queue.shift.children
+
+        # end
+        found? = false
+        queue = [node]
+
+        while(found? == false)
+
+      
+
+        queue[0].children.each do |child|
+        
+            if child != target
+                queue.shift!; queue.push(child)
+                found == false 
+            end
+
+
+        end
+        
+
+
+
+
+    end
 end
