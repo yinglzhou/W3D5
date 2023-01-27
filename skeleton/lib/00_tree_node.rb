3b098abba3c3ebb0cc45cbcd616f5ea74283b3f2
Kelly Zhou <yinglinzhou0@gmail.com>
2023-01-27 13:56:55 -0500

    add parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
 
+
+    end
+
+    def parent=(node)
+        @parent = node 
+        self.children << node unless @parent != nil
+    end
 end
