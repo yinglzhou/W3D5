0d25c3cae12b989c2a5908b65d0d0c4855a2bb02
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-01-30 15:13:55 -0500

    new position

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,7 +1,8 @@
 class KnightPathFinder
+    attr_reader :considered_positions
     def initialize(position)
         @position = position
-        @considered_positions = []
+        @considered_positions = [position]
     end
 
     def self.valid_moves(position)
@@ -32,6 +33,27 @@ class KnightPathFinder
         valid_move_arr
     end
 
+    def new_positions(position)
+
+        final_arr = []
+
+        new_moves = KnightPathFinder.valid_moves(position)
+
+        i = 0
+            while( i < new_moves.length)
+                if @considered_positions.include?(new_moves[i])
+                   next 
+                   i+=1
+                else
+                    @considered_positions.push(new_moves[i])
+                end
+                i+=1
+            end
+
+            return final_arr
+
+    end
+
 
 
 
@@ -39,5 +61,12 @@ class KnightPathFinder
 
 end
 
-p knight1 = KnightPathFinder.new([0,0])
-p KnightPathFinder.valid_moves([2, 1])
+
+p knight = KnightPathFinder.new([0,0])
+p KnightPathFinder.valid_moves([0,0])
+
+
+p knight.considered_positions
+
+
+
